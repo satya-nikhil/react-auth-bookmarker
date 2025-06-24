1819867fc628b2222774b1389882e153b3c86dde
Satya Nikhil <satyanikhil.nandigam@sasi.ac.in>
2025-06-24 17:45:44 +0530

    Updated footer with my name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 p-4 text-center text-gray-300">
-      <div>© 2024 Bookmark Manager. All rights reserved.
+      <div>© 2025 Bookmark Manager. All rights reserved.
          developed by <a className='text-red-700 'target='_blank' rel="noreferrer"  href='https://github.com/satya-nikhil/react-auth-bookmarker'>satya-nikhil</a>
       </div>
     </footer>
